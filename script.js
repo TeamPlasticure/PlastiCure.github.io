8e0f38625c9654c0df69a34d787a3b1aabff0efe
TeamPlasticure <TeamPlastiCure@gmail.com>
2025-05-18 01:19:06 +0400

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  gsap.utils.toArray('.fade-in').forEach((elem) => {
-    gsap.to(elem, {
+  gsap.utils.toArray('.fade-in').forEach((section) => {
+    gsap.to(section, {
       scrollTrigger: {
-        trigger: elem,
-        start: 'top 85%',
-        toggleActions: 'play none none reverse',
+        trigger: section,
+        start: "top 80%",
+        toggleActions: "play none none reverse",
       },
       duration: 1,
       opacity: 1,
       y: 0,
-      ease: 'power3.out',
-      stagger: 0.2,
+      ease: "power3.out",
     });
   });
 });
